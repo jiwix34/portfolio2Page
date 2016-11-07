1031f62c14fceac6730dfacc2d4c01ebaf4792d4
jiwix34 <friedblattjimmybeweb@outlook.fr>
2016-11-07 13:41:07 +0100

    commit initial

---
+++ javascript/centrage.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  var hauteurfenetre = $(window).height();
+  var grandeurformulaire = $(".container").height();
+  var resultat = hauteurfenetre-grandeurformulaire;
+  $(".container").css("margin-top", resultat/2+"px");
+
+});
---
+++ javascript/grandeur.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  var hauteurfenetre = $(window).height();
+  $(".contactphp").height(hauteurfenetre+"px");
+
+});
